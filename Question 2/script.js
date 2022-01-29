790655d30d3635be33d55b759416ce72b7ff2cf9
Mohamed Ali NS <mohamedali.elmac@gmail.com>
2022-01-29 23:04:15 +0530

    Question 2 - Display All Country Flags

---
+++ Question 2/script.js
@@ -0,0 +1,16 @@
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all")
+request.send ()
+
+request.onload = function()
+{
+    if (request.status == 200) {
+     let a = JSON.parse(request.responseText)
+    }
+    else {
+        request.onerror = function() 
+        {
+        console.log("error");
+        }
+    }
+}
