2162887ae17f96f0a28da3709fd0038ae9373430
Mohamed Ali NS <mohamedali.elmac@gmail.com>
2022-01-30 12:47:29 +0530

    Question 2

--- Question 2/script.js
+++ Question 2/script.js
@@ -1,10 +1,10 @@
 let request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/all")
-request.send ()
+request.open("GET", "https://restcountries.com/v3.1/all");
+request.send ();
 request.onload = function()
 {
     if (request.status == 200) {
-     let a = JSON.parse(request.responseText)
+     let a = JSON.parse(request.responseText);
     }
     else {
         request.onerror = function() 
