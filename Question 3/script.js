574b020211a91c86ff421223e357e551e0141837
Mohamed Ali NS <mohamedali.elmac@gmail.com>
2022-01-30 21:00:26 +0530

    Question 3

---
+++ Question 3/script.js
@@ -0,0 +1,16 @@
+let request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all");
+request.send ();
+request.onload = function()
+{
+     let a = JSON.parse(request.responseText);
+     a.map((b)=> {
+        console.log(b.name.common);
+        })
+        a.map((c)=> {
+            console.log(c.region);
+        })
+        a.map((d)=> {
+            console.log(d.subregion);
+        })
+}
