cfb51c5a541a7d5af1c89ddef1cea3d36c900932
Mohamed Ali NS <mohamedali.elmac@gmail.com>
2022-03-02 19:54:56 +0530

    medium tasks

--- Question 4/script.js
+++ Question 4/script.js
@@ -1,1 +1,27 @@
 // Task 1: Simple Programs todo for variables
+// 1. Declare four variables without assigning values and print them in console
+let a;
+let b;
+let c;
+let d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+// 2. How to get value of the variable myvar as output
+var myvar = 10;
+console.log(myvar);
+// 3. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+    let firstName = "Mohamed";
+    let lastName = "Ali";
+    let maritalStatus = "Married";
+    let country = "India";
+    let age = 34;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(maritalStatus);
+    console.log(country);
+    console.log(age);
+// 4. Declare variables to store your first name, last name, marital status, country and age in a single line
+let array = [firstName = "Mohamed", lastName = "Ali", maritalStatus = "Married", country = "India", age = 34];
+console.log(array);
